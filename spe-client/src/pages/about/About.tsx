bcae0400c37e75b845e70819a71a1e918066536d
Maria Fernanda <mafenandaup@gmail.com>
2025-07-04 16:42:26 -0300

    docs: delete unused items

--- spe-client/src/pages/about/About.tsx
+++ spe-client/src/pages/about/About.tsx
@@ -3,7 +3,19 @@ const About = () => {
   return (
       <>
           <section className="content-section" id="about">
-              <h1>About Section</h1>
+              <div className="about-content">
+                  <img src=".." alt="" />
+                  <div className="about-textbox">
+                      <h1>Quem somos nós?</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus inventore sapiente optio laboriosam deleniti quod sunt cumque culpa, officia eligendi labore quidem assumenda accusamus accusantium atque id minus similique nesciunt.</p>
+                  </div>
+              </div>
+              <div className="about-content">
+                  <div className="about-textbox">
+                      <h1>Quem somos nós?</h1>
+                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus inventore sapiente optio laboriosam deleniti quod sunt cumque culpa, officia eligendi labore quidem assumenda accusamus accusantium atque id minus similique nesciunt.</p>
+                  </div>
+              </div>
           </section>
       </>
   )
--- spe-client/src/pages/home/Hero.tsx
+++ spe-client/src/pages/home/Hero.tsx
@@ -1,13 +1,18 @@
 
 import '../../styles/hero.css'
 import Navbar from '../../components/navbar'
+import { faWhatsapp } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 const Hero = () => {
   return (
     <>
        <Navbar />
-      <section className="content-section">
+      <section className="content-section hero-bg">
         <div className="hero-contents">
-          
+<h1>Insira título muito atraente</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non debitis ad, quos ratione, eum quod et cupiditate totam fugiat molestias voluptate dolorem voluptates repudiandae deserunt ipsam alias aperiam facilis voluptatum?</p>
+       <button><FontAwesomeIcon icon={faWhatsapp} /> Matricule-se agora</button>
         </div>
       </section>
     </>
